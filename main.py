a290493a4e8c5eba15f6acdea827356f8fbaf49b
saurabhlodhi-tech <saurabhlodhi247@gmail.com>
2026-08-25 11:36:50 +0530

    Add if,elif and else condition

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+while True:
+ user_choice = input("Enter your choice :").lower()
+
+ computer_choice = random.choice(choices)
+
+ if user_choice == computer_choice:
+    print("Draw!")
+ elif user_choice == "scissors" and computer_choice == "paper":
+    print("You Win")
+ elif user_choice =="rock" and computer_choice == "scissors":
+    print("You Win")
+ elif user_choice =="paper" and computer_choice == "rock":
+    print("You Win")
+ else:
+    print("Computer wins !")
+
+
